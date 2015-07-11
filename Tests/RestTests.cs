e73f04b0daf73912b0622bb48bdb2597da2e4a0e
Michał Jawulski <m.jawulski@sente.pl>
2015-07-11 11:34:08 +0200

    Naive

--- Tests/RestTests.cs
+++ Tests/RestTests.cs
@@ -66,7 +66,7 @@ namespace Tests
       'stack':1000,
       'status':'active',
       'bet':0,
-      'hole_cards':[],
+      'hole_cards':[{'rank':2, 'suit':'spades'}, {'rank':'2', 'suit':'spades'}],
       'version':'Version name 1',
       'id':0
     },
@@ -85,7 +85,8 @@ namespace Tests
   'dealer':0,
   'community_cards':[],
   'current_buy_in':0,
-  'pot':0
+  'pot':0,
+  'in_action':0
 }");
             
 
--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -1,16 +1,24 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 
 namespace Nancy.Simple
 {
 	public static class PokerPlayer
 	{
-		public static readonly string VERSION = "Version 1";
+		public static readonly string VERSION = "Version 2";
 
 		public static int BetRequest(JObject gameState)
 		{
 			//TODO: Use this method to return the value You want to bet
+		    GameState gs = GetGameStateFromJObject(gameState);
+			StartingHands sh = new StartingHands();
+		    Card[] cards = gs.players[gs.in_action].hole_cards.Take(2).ToArray();
+            Tuple<Card,Card> firstHand = new Tuple<Card, Card>(cards[0],cards[1]);
 
+		    if (sh.RankHand(firstHand) == StaringHandsRank.Strong) return GetCallAmount(gs);
+             
 			return 0;
 		}
 
@@ -23,15 +31,15 @@ namespace Nancy.Simple
 			return gameState.ToObject<GameState> ();
 		}
 
-	    public static int GetCallAmount(GameState gameState)
-	    {
-	        return gameState.current_buy_in - gameState.players[gameState.in_action].bet;
-	    }
+		public static int GetCallAmount(GameState gameState)
+		{
+			return gameState.current_buy_in - gameState.players[gameState.in_action].bet;
+		}
 
-	    public static int GetAllInAmount(GameState gameState)
-	    {
-	        return gameState.players[gameState.in_action].stack;
-	    }
+		public static int GetAllInAmount(GameState gameState)
+		{
+			return gameState.players[gameState.in_action].stack;
+		}
 	}
 }
 
