94d1a5bcc22b98222aa86176635b15f52bfcff6d
Michał Jawulski <m.jawulski@sente.pl>
2015-07-11 10:15:48 +0200

    GameState

---
+++ src/GameState.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Nancy.Simple
+{
+	public class GameState
+	{
+		public Player[] players { get; set; }
+		public string tournament_id { get; set; }
+		public string game_id { get; set; }
+		public int round { get; set; }
+		public int bet_index { get; set; }
+		public int small_blind { get; set; }
+		public int orbits { get; set; }
+		public int dealer { get; set; }
+		public Card[] community_cards { get; set; }
+		public int current_buy_in { get; set; }
+		public int pot { get; set; }
+	}
+
+	public class Player
+	{
+		public string name { get; set; }
+		public int stack { get; set; }
+		public string status { get; set; }
+		public int bet { get; set; }
+		public Card[] hole_cards { get; set; }
+		public string version { get; set; }
+		public int id { get; set; }
+	}
+
+	public class Card
+	{
+		public string rank { get; set; }
+		public string suit { get; set; }
+	}
+}
+
