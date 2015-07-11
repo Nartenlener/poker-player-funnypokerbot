b42895f7e0fdec6b7c3d468d4ba753399f032e1a
bartq <bci@poczta.onet.pl>
2015-07-11 10:34:01 +0200

    test to check if bet_request returns something legal

--- Tests/RestTests.cs
+++ Tests/RestTests.cs
@@ -36,6 +36,65 @@ namespace Tests
             var content = response.Content; // raw content as string
 			Assert.AreEqual(content, PokerPlayer.VERSION);
         }
+
+		[Test()]
+		public void ShouldReturnResponseToCheck()
+		{
+			var client = new RestClient(App.STAGINGHOST);
+
+			var request = new RestRequest("/", Method.POST);
+			request.AddParameter("action", "check");
+
+			// execute the request
+			IRestResponse response = client.Execute(request);
+			var content = response.Content; // raw content as string
+			Assert.AreEqual(content, "OK");
+		}
+
+        [Test()]
+        public void ShouldReturnResponseToBetRequest()
+        {
+            var client = new RestClient(App.STAGINGHOST);
+
+            var request = new RestRequest("/", Method.POST);
+            request.AddParameter("action", "bet_request");
+            request.AddParameter("game_state", @"{
+  'players':[
+    {
+      'name':'Player 1',
+      'stack':1000,
+      'status':'active',
+      'bet':0,
+      'hole_cards':[],
+      'version':'Version name 1',
+      'id':0
+    },
+    {
+      'name':'Player 2',
+      'stack':1000,
+      'status':'active',
+      'bet':0,
+      'hole_cards':[],
+      'version':'Version name 2',
+      'id':1
+    }
+  ],
+  'small_blind':10,
+  'orbits':0,
+  'dealer':0,
+  'community_cards':[],
+  'current_buy_in':0,
+  'pot':0
+}");
+            
+
+            // execute the request
+            IRestResponse response = client.Execute(request);
+            var content = response.Content; // raw content as string
+            Assert.IsFalse(string.IsNullOrWhiteSpace(content));
+            int val;
+            Assert.IsTrue(int.TryParse(content, out val));
+        }
 	}
 }
 
