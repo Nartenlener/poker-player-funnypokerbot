cb55fb2b59d817034103e8ba2c0335ac654eb903
bartq <bci@poczta.onet.pl>
2015-07-11 11:43:16 +0200

    test

--- src/StartingHands.cs
+++ src/StartingHands.cs
@@ -11,7 +11,10 @@ namespace Nancy.Simple
 
         public StaringHandsRank RankHand(Tuple<Card,Card> inCard)
         {
-
+            if(inCard.Item2.GreaterThan(inCard.Item1))
+            {
+                inCard = new Tuple<Card, Card>(inCard.Item2, inCard.Item1);
+            }
             if(inCard.Item1.rank == inCard.Item2.rank)
                 return StaringHandsRank.Strong;
             return StaringHandsRank.Fold;
