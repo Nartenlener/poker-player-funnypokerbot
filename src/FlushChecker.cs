7738859c320366f210dae6e45272ef4882184472
Udgin Pyl <gromkaktus@gmail.com>
2015-07-11 17:06:44 +0200

    Fixed when there is more than 5 cards

--- src/FlushChecker.cs
+++ src/FlushChecker.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Nancy.Simple
 {
@@ -8,12 +10,11 @@ namespace Nancy.Simple
         {
             if (cards.Count() == 0) return false;
             if (cards.Count() == 1) return true;
-            var suit = cards[0].suit;
-            foreach (var card in cards.Skip(1))
-            {
-                if (card.suit != suit)
-                    return false;
-            }
+            if (cards.Count() < 5)
+                return cards.Select(x => x.suit).Distinct().Count() == 1;
+            var suitsCount = cards.GroupBy(x => x.suit).Select(x => new { x.Key, count = x.Count() });
+            if (suitsCount.Any(x => x.count > 4))
+                return true;
             return true;
         }
     }
