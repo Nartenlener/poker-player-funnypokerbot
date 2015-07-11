561d5860e6a7a644a777d807e74c6e406cba7404
Michał Jawulski <m.jawulski@sente.pl>
2015-07-11 10:45:12 +0200

    New GameState with all props

--- src/GameState.cs
+++ src/GameState.cs
@@ -4,28 +4,30 @@ namespace Nancy.Simple
 {
 	public class GameState
 	{
-		public Player[] players { get; set; }
 		public string tournament_id { get; set; }
 		public string game_id { get; set; }
 		public int round { get; set; }
 		public int bet_index { get; set; }
 		public int small_blind { get; set; }
-		public int orbits { get; set; }
-		public int dealer { get; set; }
-		public Card[] community_cards { get; set; }
 		public int current_buy_in { get; set; }
 		public int pot { get; set; }
+		public int minimum_raise { get; set; }
+		public int dealer { get; set; }
+		public int orbits { get; set; }
+		public int in_action { get; set; }
+		public Player[] players { get; set; }
+		public Card[] community_cards { get; set; }
 	}
 
 	public class Player
 	{
+		public int id { get; set; }
 		public string name { get; set; }
-		public int stack { get; set; }
 		public string status { get; set; }
+		public string version { get; set; }
+		public int stack { get; set; }
 		public int bet { get; set; }
 		public Card[] hole_cards { get; set; }
-		public string version { get; set; }
-		public int id { get; set; }
 	}
 
 	public class Card
@@ -33,5 +35,6 @@ namespace Nancy.Simple
 		public string rank { get; set; }
 		public string suit { get; set; }
 	}
+
 }
 
