25324d4aecd05a44a6104c9faf21705ca991c805
Udgin Pyl <gromkaktus@gmail.com>
2015-07-11 10:43:21 +0200

    Added positive test for GetGameStateFromJObjectNotNull

--- Tests/PokerPlayerTests.cs
+++ Tests/PokerPlayerTests.cs
@@ -52,5 +52,32 @@ namespace Tests
 
             Assert.IsNotNull(inputObj);
         }
+
+        [Test]
+        public void GetGameStateFromJObjectPositive()
+        {
+            var inputObj = JObject.Parse(input);
+            var result = PokerPlayer.GetGameStateFromJObject(inputObj);
+
+            Assert.AreEqual(result.players.Count(), 2);
+            Assert.AreEqual(result.players[0].name, "Player 1");
+            Assert.AreEqual(result.players[0].stack, 1000);
+            Assert.AreEqual(result.players[0].status, "active");
+            Assert.AreEqual(result.players[0].bet, 0);
+            Assert.AreEqual(result.players[0].hole_cards.Count(), 0);
+            Assert.AreEqual(result.players[0].version, "Version name 1");
+            Assert.AreEqual(result.players[0].id, 0);
+
+            Assert.AreEqual(result.tournament_id, "550d1d68cd7bd10003000003");
+            Assert.AreEqual(result.game_id, "550da1cb2d909006e90004b1");
+            Assert.AreEqual(result.round, 0);
+            Assert.AreEqual(result.bet_index, 0);
+            Assert.AreEqual(result.small_blind, 10);
+            Assert.AreEqual(result.orbits, 0);
+            Assert.AreEqual(result.dealer, 0);
+            Assert.AreEqual(result.community_cards.Count(), 0);
+            Assert.AreEqual(result.current_buy_in, 0);
+            Assert.AreEqual(result.pot, 0);
+        }
     }
 }
