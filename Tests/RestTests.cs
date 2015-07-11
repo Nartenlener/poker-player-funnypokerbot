492d0bcceccad1cceefdfcb7d20d4740e7f05a90
bartq <bci@poczta.onet.pl>
2015-07-11 10:08:13 +0200

    rest tests

---
+++ Tests/RestTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using Nancy.Simple;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Tests
+{
+	[TestFixture ()]
+	public class RestTests
+	{
+		
+		public void ShouldBeAlive() {
+		}
+	}
+}
+
