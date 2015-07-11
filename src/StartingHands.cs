a1e10d16553f5e073d69ac1783f67ee73cdc131a
bartq <bci@poczta.onet.pl>
2015-07-11 11:13:41 +0200

    first version

---
+++ src/StartingHands.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nancy.Simple
+{
+    public enum StaringHandsRank { Strong, Mid, Late, Fold }
+    public class StartingHands
+    {
+
+        public StaringHandsRank RankHand(Tuple<Card,Card> inCard)
+        {
+            if(inCard.Item1.rank == inCard.Item2.rank)
+                return StaringHandsRank.Strong;
+            return StaringHandsRank.Fold;
+        }
+    }
+}
